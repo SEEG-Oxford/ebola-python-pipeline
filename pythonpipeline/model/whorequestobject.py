cd92b0a9ec31c30a386f256f990427e92ab83ab1
Andrew Schofield <andrew.schofield@tessella.com>
2015-02-17 10:56:12 +0000

    Added new WHORequest object

---
+++ pythonpipeline/model/whorequestobject.py
@@ -0,0 +1,14 @@
+__author__ = 'andrew.schofield@tessella.com'
+
+
+class WHORequestObject(object):
+    def __init__(self):
+        self.format = None
+        self.target = None
+        self.targetcolumns = None
+        self.profile = None
+        self.countries = None
+        self.location = None
+        self.datapackageid = None
+        self.indicatortype = None
+        self.sex = None
