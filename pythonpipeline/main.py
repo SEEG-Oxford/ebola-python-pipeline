77685eef3e8e1ddfe76147f32486eaf1515b0b03
Andrew Schofield <andrew.schofield@tessella.com>
2015-02-17 15:18:44 +0000

    Download file from WHO and (temporarily) save it to disk

---
+++ pythonpipeline/main.py
@@ -0,0 +1,34 @@
+__author__ = 'andrew.schofield@tessella.com'
+
+import model.whorequestobject
+import services.WHODataExtractor
+
+
+class Pipeline(object):
+    def __init__(self):
+        self.whodataextractor = None
+
+    def main(self):
+        requestobject = model.whorequestobject.WHORequestObject()
+        requestobject.format = "data-text.csv"
+        requestobject.target = "EBOLA_MEASURE"
+        requestobject.targetcolumns = ["CASES", "DEATHS"]
+        requestobject.profile = "text"
+        requestobject.countries = ["GIN", "UNSPECIFIED", "LBR", "UNSPECIFIED", "SLE", "UNSPECIFIED"]
+        requestobject.location = ["-"]
+        requestobject.datapackageid = ["2014-11-14"]
+        requestobject.indicatortype = ["SITREP_CUMULATIVE", "SITREP_CUMULATIVE_21_DAYS"]
+        requestobject.sex = "-"
+
+        self.whodataextractor = services.WHODataExtractor.WHODataExtractor(requestobject)
+
+        data = self.whodataextractor.downloadfromwhowebsite()
+
+        with open("download.file", "w") as file:
+            file.write(data.read())
+            file.close()
+
+if __name__ == '__main__':
+    Pipeline = Pipeline()
+
+    Pipeline.main()
