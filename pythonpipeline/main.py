36ea3f4c3362102dc558b8f63d646a1ece51ffa9
Andrew Schofield <andrew.schofield@tessella.com>
2015-02-18 16:04:29 +0000

    Add ability to specify the date and an R file as parameters on the command line

--- pythonpipeline/main.py
+++ pythonpipeline/main.py
@@ -2,18 +2,41 @@ __author__ = 'andrew.schofield@tessella.com'
 
 import model.WHORequestObject
 import services.WHODataExtractor
+from subprocess import call
+import sys
+import getopt
 
 
 class Pipeline(object):
     def __init__(self):
         self.whodataextractor = None
 
-    def main(self):
-        self.downloadforcountry("GIN")
-        #self.downloadforcountry("LBR")
-        #self.downloadforcountry("SLE")
+    def main(self, argv):
+        date = None
+        rsourcefile = ''
+        try:
+            opts, args = getopt.getopt(argv, "hd:r:", ["date=", "rfile="])
+        except getopt.GetoptError:
+            print 'main.py -d <date> -r <R source filename>'
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                print 'main.py -d <date> -r <R source filename>'
+                sys.exit()
+            elif opt in ("-d", "--date"):
+                date = arg
+            elif opt in ("-r", "--rfile"):
+                rsourcefile = arg
 
-    def downloadforcountry(self, countryname):
+        self.downloadforcountry("GIN", date)
+        self.downloadforcountry("LBR", date)
+        self.downloadforcountry("SLE", date)
+
+        # windows specific at the moment
+        # need to replace with a platform agnostic call to run R
+        output = call(["C:\\Program Files\\R\\R-3.1.2\\bin\\R.exe", "--no-save", "--file=" + rsourcefile])
+
+    def downloadforcountry(self, countryname, date):
         requestobject = model.WHORequestObject.WHORequestObject()
         requestobject.format = "data-verbose.csv"
         requestobject.target = "EBOLA_MEASURE"
@@ -21,7 +44,7 @@ class Pipeline(object):
         requestobject.profile = "verbose"
         requestobject.countries = [countryname]
         requestobject.location = ["*"]
-        requestobject.datapackageid = ["2015-02-11"]
+        requestobject.datapackageid = [date]
         requestobject.indicatortype = ["SITREP_NEW"]
         requestobject.sex = "-"
 
@@ -36,4 +59,4 @@ class Pipeline(object):
 if __name__ == '__main__':
     Pipeline = Pipeline()
 
-    Pipeline.main()
+    Pipeline.main(sys.argv[1:])
