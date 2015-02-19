6d3bab5821ebc98fc8e8435405fc3d26c983476d
Andrew Schofield <andrew.schofield@tessella.com>
2015-02-19 09:45:17 +0000

    Add ability command line option for output directory (applies to downloaded csv file, processed csv file and Rplot.pdf file)

--- pythonpipeline/main.py
+++ pythonpipeline/main.py
@@ -5,6 +5,7 @@ import services.WHODataExtractor
 from subprocess import call
 import sys
 import getopt
+import os.path
 
 
 class Pipeline(object):
@@ -14,29 +15,33 @@ class Pipeline(object):
     def main(self, argv):
         date = None
         rsourcefile = ''
+        outputdir = ''
         try:
-            opts, args = getopt.getopt(argv, "hd:r:", ["date=", "rfile="])
+            opts, args = getopt.getopt(argv, "hd:r:o:", ["date=", "rfile=", "outputdir="])
         except getopt.GetoptError:
-            print 'main.py -d <date> -r <R source filename>'
+            print 'main.py -d <date> -r <R source filename> -o <output directory>'
             sys.exit(2)
         for opt, arg in opts:
             if opt == '-h':
-                print 'main.py -d <date> -r <R source filename>'
+                print 'main.py -d <date> -r <R source filename> -o <output directory>'
                 sys.exit()
             elif opt in ("-d", "--date"):
                 date = arg
             elif opt in ("-r", "--rfile"):
                 rsourcefile = arg
+            elif opt in ("-o", "--outputdir"):
+                outputdir = arg
 
-        self.downloadforcountry("GIN", date)
-        self.downloadforcountry("LBR", date)
-        self.downloadforcountry("SLE", date)
+        self.downloadforcountry("GIN", date, outputdir)
+        self.downloadforcountry("LBR", date, outputdir)
+        self.downloadforcountry("SLE", date, outputdir)
+        self.downloadforcountry("SLE", date, outputdir)
 
         # windows specific at the moment
         # need to replace with a platform agnostic call to run R
-        output = call(["C:\\Program Files\\R\\R-3.1.2\\bin\\R.exe", "--no-save", "--file=" + rsourcefile])
+        output = call(["C:\\Program Files\\R\\R-3.1.2\\bin\\R.exe", "--no-save", "--file=" + os.path.abspath(rsourcefile)], cwd=outputdir)
 
-    def downloadforcountry(self, countryname, date):
+    def downloadforcountry(self, countryname, date, outputdir):
         requestobject = model.WHORequestObject.WHORequestObject()
         requestobject.format = "data-verbose.csv"
         requestobject.target = "EBOLA_MEASURE"
@@ -52,7 +57,7 @@ class Pipeline(object):
 
         data = self.whodataextractor.downloadfromwhowebsite()
 
-        with open(countryname + ".csv", "w") as file:
+        with open(os.path.join(outputdir, countryname + ".csv"), "w") as file:
             file.write(data.read())
             file.close()
 
