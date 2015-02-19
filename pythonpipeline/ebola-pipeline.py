eac63957e503735190da14c685177fe0839ca4dd
Andrew Schofield <andrew.schofield@tessella.com>
2015-02-19 13:54:31 +0000

    Update pipeline to automatically pull down the lastest data by parsing the latest data page

--- pythonpipeline/ebola-pipeline.py
+++ pythonpipeline/ebola-pipeline.py
@@ -6,6 +6,8 @@ from subprocess import call
 import sys
 import getopt
 import os.path
+import urllib2
+import re
 
 
 class Pipeline(object):
@@ -13,33 +15,30 @@ class Pipeline(object):
         self.whodataextractor = None
 
     def main(self, argv):
-        date = None
         rsourcefile = ''
         outputdir = ''
         try:
-            opts, args = getopt.getopt(argv, "hd:r:o:", ["date=", "rfile=", "outputdir="])
+            opts, args = getopt.getopt(argv, "hr:o:", ["rfile=", "outputdir="])
         except getopt.GetoptError:
-            print 'ebola-pipeline.py -d <date> -r <R source filename> -o <output directory>'
+            print 'ebola-pipeline.py -r <R source filename> -o <output directory>'
             sys.exit(2)
         for opt, arg in opts:
             if opt == '-h':
-                print 'ebola-pipeline.py -d <date> -r <R source filename> -o <output directory>'
+                print 'ebola-pipeline.py -r <R source filename> -o <output directory>'
                 sys.exit()
-            elif opt in ("-d", "--date"):
-                date = arg
             elif opt in ("-r", "--rfile"):
                 rsourcefile = arg
             elif opt in ("-o", "--outputdir"):
                 outputdir = arg
 
-        self.downloadforcountry("GIN", date, outputdir)
-        self.downloadforcountry("LBR", date, outputdir)
-        self.downloadforcountry("SLE", date, outputdir)
+        self.downloadforcountry("GIN", outputdir)
+        self.downloadforcountry("LBR", outputdir)
+        self.downloadforcountry("SLE", outputdir)
 
         # as long as R is on your path this should work
         output = call(["R", "--silent", "--slave", "--vanilla", "--file=" + os.path.abspath(rsourcefile)], cwd=outputdir)
 
-    def downloadforcountry(self, countryname, date, outputdir):
+    def downloadforcountry(self, countryname, outputdir):
         requestobject = model.WHORequestObject.WHORequestObject()
         requestobject.format = "data-verbose.csv"
         requestobject.target = "EBOLA_MEASURE"
@@ -47,7 +46,7 @@ class Pipeline(object):
         requestobject.profile = "verbose"
         requestobject.countries = [countryname]
         requestobject.location = ["*"]
-        requestobject.datapackageid = [date]
+        requestobject.datapackageid = [self.findlatestavailabledateforcountry(countryname)]
         requestobject.indicatortype = ["SITREP_NEW"]
         requestobject.sex = "-"
 
@@ -58,6 +57,16 @@ class Pipeline(object):
         with open(os.path.join(outputdir, countryname + ".csv"), "w") as file:
             file.write(data.read())
             file.close()
+
+
+    def findlatestavailabledateforcountry(self, country):
+        date = None
+        data = urllib2.urlopen("http://apps.who.int/gho/data/node.ebola-sitrep.ebola-country-" + country + "-latest?lang=en")
+        # regular expression to pull out the latest packageid from the page
+        m = re.search("DATAPACKAGEID:(.*?);", data.read())
+        date = m.group(1)
+        print "Latest data found for " + country + ": " + date
+        return date
 
 if __name__ == '__main__':
     Pipeline = Pipeline()
