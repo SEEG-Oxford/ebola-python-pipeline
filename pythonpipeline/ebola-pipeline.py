d40a45863f9224ea45be91bea66ca4ea158fc18b
Andrew Schofield <andrew.schofield@tessella.com>
2015-04-20 15:12:14 +0100

    Added code to pipeline to process global risk data

--- pythonpipeline/ebola-pipeline.py
+++ pythonpipeline/ebola-pipeline.py
@@ -94,6 +94,10 @@ class Pipeline(object):
 
         output = call(["R", "--silent", "--slave", "--vanilla",
                        "--file=" + os.path.abspath(rdir + "/regional_results/plotWeightedMap.R")], cwd=rdir + "/regional_results")
+
+        output = call(["R", "--silent", "--slave", "--vanilla",
+                       "--file=" + os.path.abspath(rdir + "/global_results/plotMap.R")], cwd=rdir + "/global_results")
+
         if publishrepo != '':
             # update the repository
             # WARNING: This needs to be run from a shell that can access a valid private key for the repository
@@ -119,6 +123,25 @@ class Pipeline(object):
             call(["git", "add", repopath + "/local-risk.md"], cwd=repopath)
 
             call(["git", "add", repopath + "/weightings.csv"], cwd=repopath)
+
+            os.chdir(os.path.abspath(rdir + "/global_results"))
+            for file in glob.glob("*.png"):
+                filename = os.path.basename(file)
+                shutil.copy(file, repopath + "/images/" + filename)
+                call(["git", "add", repopath + "/images/" + filename], cwd=repopath)
+
+            # basic markdown editing
+            s = open(repopath + "/global-risk.md", "r")
+            w = open(repopath + "/global-risk.md1", "w")
+            for line in s.readlines():
+                line = re.sub(r"(### Latest data as of )(.*)", r"\1 " + str(datetime.date.today()), line)
+                w.write(line)
+            s.close()
+            w.close()
+            os.remove(repopath + "/global-risk.md")
+            os.rename(repopath + "/global-risk.md1", repopath + "/global-risk.md")
+
+            call(["git", "add", repopath + "/global-risk.md"], cwd=repopath)
 
             call(["git", "commit", "-m", "Updated WHO data and regional risk plots as of " + str(datetime.date.today())], cwd=repopath)
 
