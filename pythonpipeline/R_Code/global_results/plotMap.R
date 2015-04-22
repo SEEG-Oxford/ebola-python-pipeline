a752079ee62a37baabb75deba81c5422351b049e
Andrew Schofield <andrew.schofield@tessella.com>
2015-04-22 11:09:49 +0100

    Add line breaks in title for small plots

--- pythonpipeline/R_Code/global_results/plotMap.R
+++ pythonpipeline/R_Code/global_results/plotMap.R
@@ -91,14 +91,14 @@ source("calculateRisks.R")
 countrycodes <- as.vector(all$country.1)
 
 risks <- data.frame(country=countrycodes, risk=all$importation_risk)
-plotRisks(risks, countries, adm0, "global_Overall_prediction", "Global relative risk of Ebola importation from overall model")
+plotRisks(risks, countries, adm0, "global_Overall_prediction", "Global relative risk of Ebola importation\n from overall model")
 
 risks <- data.frame(country=countrycodes, risk=all$adjacency_relative)
-plotRisks(risks, countries, adm0, "global_Adjacency_prediction", "Global relative risk of Ebola importation from Adjacency model")
+plotRisks(risks, countries, adm0, "global_Adjacency_prediction", "Global relative risk of Ebola importation\n from Adjacency model")
 
 risks <- data.frame(country=countrycodes, risk=all$gravity_relative)
-plotRisks(risks, countries, adm0, "global_Gravity_prediction", "Global relative risk of Ebola importation from Gravity model")
+plotRisks(risks, countries, adm0, "global_Gravity_prediction", "Global relative risk of Ebola importation\n from Gravity model")
 
 risks <- data.frame(country=countrycodes, risk=all$migration_relative)
-plotRisks(risks, countries, adm0, "global_Migration_prediction", "Global relative risk of Ebola importation from Migration model")
+plotRisks(risks, countries, adm0, "global_Migration_prediction", "Global relative risk of Ebola importation\n from Migration model")
 
